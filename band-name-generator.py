3acd0a1e69a88048d038b892e1afffe3c5a70db8
Bhoami Khona <50435319+bhoamikhona@users.noreply.github.com>
2021-04-01 19:13:09 -0500

    Band Name Generator
    Printing
    Commenting
    Debugging
    String Manipulation
    Variables

---
+++ band-name-generator.py
@@ -0,0 +1,8 @@
+# Program greeting.
+print("Welcome to the Band Name Generator.")
+# User input for city.
+city = input("What's the name of the city you grew up in?\n")
+# User input for pet's name.
+pet_name = input("What's your pet's name?\n")
+# Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + city + " " + pet_name)
